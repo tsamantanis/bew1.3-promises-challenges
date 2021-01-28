42d1709ccfe7b1d9232075f4532e0c02ceac674b
tsamantanis <philip.tsamantanis@icloud.com>
2021-01-27 17:08:12 -0800

    4 tab indentation

--- challenges/challenge4.js
+++ challenges/challenge4.js
@@ -1,38 +1,38 @@
 /*
- *******************************************************************************
- * INSTRUCTIONS:
- * Follow the steps below and answer the discusssion questions that follow.
- * 
- * 1. Read over the code that follows. `makePromise` is a function that makes
- *    an API call and returns a Promise containing the result. Uncomment code
- *    block #1 and run the code. What happens? (HINT: You may need to run
- *    `npm init` first.)
- * 
- * 
- * 2. Sometimes, when making API calls, we want to make a bunch of calls in
- *    parallel and don't care in what order they resolve. (In other words, they
- *    don't depend on each other.)
- * 
- *    Uncomment code block #2 and run the code. What happens? What advantages 
- *    does `Promise.all` give us when dealing with promises?
- * 
- * 
- * 3. Make another variable `planet1Promise` and assign to it the result of
- *    calling `makePromise` with the URL `https://swapi.co/api/planets/1`.
- *    Add it to the array passed to `Promise.all`, then print the name of the
- *    returned planet inside the `.then()` callback.
- * 
- * 
- *******************************************************************************
- */
+*******************************************************************************
+* INSTRUCTIONS:
+* Follow the steps below and answer the discusssion questions that follow.
+*
+* 1. Read over the code that follows. `makePromise` is a function that makes
+*    an API call and returns a Promise containing the result. Uncomment code
+*    block #1 and run the code. What happens? (HINT: You may need to run
+*    `npm init` first.)
+*
+*
+* 2. Sometimes, when making API calls, we want to make a bunch of calls in
+*    parallel and don't care in what order they resolve. (In other words, they
+*    don't depend on each other.)
+*
+*    Uncomment code block #2 and run the code. What happens? What advantages
+*    does `Promise.all` give us when dealing with promises?
+*
+*
+* 3. Make another variable `planet1Promise` and assign to it the result of
+*    calling `makePromise` with the URL `https://swapi.co/api/planets/1`.
+*    Add it to the array passed to `Promise.all`, then print the name of the
+*    returned planet inside the `.then()` callback.
+*
+*
+*******************************************************************************
+*/
 
 const request = require('request');
 
 
 /**
- * Makes an API request and returns a Promise containing the result.
- * @param url The url to make an API request to.
- */
+* Makes an API request and returns a Promise containing the result.
+* @param url The url to make an API request to.
+*/
 function makePromise(url) {
     return new Promise(function(resolve, reject){
         request(url, function (err, response, body) {
